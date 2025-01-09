74ed736ef6686fbc4dfde17dee38320fa71eaee4
swapnilkabir <146571656+swapnilkabir@users.noreply.github.com>
2025-01-09 08:13:09 -0500

    Update runMe.bat

--- duel/runMe.bat
+++ duel/runMe.bat
@@ -1,10 +1,13 @@
-javac Bullet.java
-javac GameFrame.java
-javac GamePanel.java
-javac Main.java
-javac Menu.java
-javac Player.java
+javac MapManager.java
 javac Score.java
+javac Player.java
+javac Bullet.java
+javac Obstacle.java
+javac Powerup.java
+javac Menu.java
+javac GamePanel.java
+javac GameFrame.java
+javac Main.java
 pause
 java Main
 pause
